576d0d7f0090be16924e0459313395747310e373
sand <prettypuppycavalier@gmail.com>
2025-03-15 13:42:24 +0800

    commit

---
+++ lib/main10.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import './res/listData.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      // 此配置已失效
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo Home Page'),
+          backgroundColor: Colors.red,
+        ),
+        body: CustomWidget(),
+      ),
+    ),
+  );
+}
+
+class CustomWidget extends StatelessWidget {
+  CustomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration( color: Colors.yellow),
+      // listView中横向布局默认子元素高度占满，设置宽度无效，需要再listView外层包裹一个容器限制高度
+      child: ListView.builder(
+        itemCount: listData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Image.network(listData[index]['imageUrl']),
+            title: Text(listData[index]['title']),
+            subtitle: Text(listData[index]['subtitle']),
+          );
+        },
+      ),
+    );
+  }
+}
